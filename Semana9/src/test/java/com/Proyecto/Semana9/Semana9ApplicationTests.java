f9136933a3c66beb80f22d5a1f343e1e874ed05d
salvagd09 <vasco14_100@hotmail.com>
2026-05-22 12:06:56 -0500

    Cosas por agregar

---
+++ Semana9/src/test/java/com/Proyecto/Semana9/Semana9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Semana9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
