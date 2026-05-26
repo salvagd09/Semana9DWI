6b177f3ba87640f3e5b59105afc06dbf515e1c67
salvagd09 <vasco14_100@hotmail.com>
2026-05-25 22:13:44 -0500

    Config que permita mi frontend desplegado

--- Semana9Backend/src/main/java/com/Proyecto/Semana9Backend/WebConfig.java
+++ Semana9Backend/src/main/java/com/Proyecto/Semana9Backend/WebConfig.java
@@ -7,6 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://semana9-dwi.vercel.app/").allowedMethods("*");
     }
 }
